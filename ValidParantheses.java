15ab4148021e867d3c2ff5c46c4e7adfdc084485
Subhadip <11837840+smaity8136@users.noreply.github.com>
2020-03-09 16:07:15 -0500

    Create ValidParantheses.java

---
+++ ValidParantheses.java
@@ -0,0 +1,69 @@
+package com.leetcode.amazone.test;
+
+import java.util.Stack;
+
+public class ValidParantheses {
+    /*
+    * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+    * Example 2:
+      Input: ()[]{}
+      Output: true
+
+      Example 3:
+      Input: (]
+      Output: false
+
+      Example 4:
+      Input: ([)]
+      Output: false
+
+      Example 5:
+      Input: {[]}
+     Output: true
+    * */
+
+    public static boolean isValid(String s) {
+
+        if(s.length()==0) return false;
+        Stack<Character> st = new Stack<>();
+        int l =s.length();
+        for(int i=0;i<i;i++){
+
+            char c = s.charAt(i);
+            switch(c){
+                case '(':
+                    st.push(')');
+                    break;
+                case '[':
+                    st.push(']');
+                    break;
+                case '{':
+                    st.push('}');
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                    if (st.size() == 0 || st.pop() != c)
+                        return false;
+                    break;
+            }
+
+        }
+        return st.size() == 0;
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        //String s = "()[]{}";
+
+        String s = "([)]";
+
+       boolean valid =  isValid(s);
+
+        System.out.println("isvalid : "+valid);
+    }
+}
