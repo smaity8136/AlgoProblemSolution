6014e5ad00a527e857c743288599df358c772847
Subhadip <11837840+smaity8136@users.noreply.github.com>
2020-03-10 16:12:51 -0500

    Create RotateImage.java

---
+++ RotateImage.java
@@ -0,0 +1,30 @@
+package com.leetcode.amazone.test;
+
+public class RotateImage {
+
+    public static int[][] rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < Math.ceil(((double) n) / 2.); j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int [] [] matrix = new int [][]{{1,2,3},
+                                        {4,5,6},
+                                        {7,8,9}};
+
+        matrix = rotate(matrix);
+
+        System.out.println(""+matrix);
+
+    }
+}
