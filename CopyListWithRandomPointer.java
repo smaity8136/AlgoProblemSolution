d24317fbd4c90cdbd53ff43e0e5a47aec85d68b5
Subhadip <11837840+smaity8136@users.noreply.github.com>
2020-03-12 16:29:19 -0500

    Create CopyListWithRandomPointer.java

---
+++ CopyListWithRandomPointer.java
@@ -0,0 +1,53 @@
+public class CopyListWithRandomPointer{
+
+
+ class RandomListNode {
+     int val;
+     RandomListNode next, random;
+     RandomListNode(int x) { this.val = x; }
+ }
+ 
+ 
+ public static copyRandomNode (RandomListNode node){
+ // create list map
+ 
+   RandomListNode curr = node;
+   
+   Map<RandomListNode,RandomListNode>map = new HashMap<RandomListNode,RandomListNode>();
+   
+   while (curr!=null){
+     map.put(curr, new RandomListNode(curr.val) )
+	 curr = curr.next;
+   
+   }
+   
+   
+  //copy next pointer
+
+  curr = node;
+
+while (curr!=null){
+  map.get(curr).next = map.get(curr.next);
+
+  curr = curr.next;  
+ }
+ 
+ 
+ curr = node;
+ 
+ while (curr!=null){
+  map.get(curr).random = curr.random;
+
+  curr = curr.next;  
+ }
+ 
+ return map.get(node);
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+
+}
